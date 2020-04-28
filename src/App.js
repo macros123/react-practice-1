eed0e9ade051b0db4176519b40a99fd8ccea4491
macros123 <hit942010@gmail.com>
2020-04-28 12:05:23 +0300

    3-parags

--- src/App.js
+++ src/App.js
@@ -1,81 +1,32 @@
 import React from 'react';
 import './App.css';
 
+const colors = ['black', 'blue', 'red', 'green']
+const colorsName = ['черный', 'синий', 'красный', 'зеленый']
+const paragraphs = ['111111111', '222222222', '33333333', '444444444444']
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      value: '',
-      users: ['Петя', 'Вася', 'Анатолий', 'Анаколий'],
-      checked: false,
-      value1: '0'
+      color: 0
     };
   }
  
-  onChangeHandler(event) {
+  changeColorHandler(event) {
     this.setState({
-      value: event.target.value
-    })
-  }
-  handleSelectChange(event) {
-    this.setState({
-      value1: event.target.value
+      color: event.target.value
     })
   }
 
-  handleSubmit(event) {
-    this.state.users.push(this.state.value)
-    this.setState({
-      users: this.state.users
-    })
-    event.preventDefault();
-  }
-
-  deleteHandler(event, index) {
-    this.state.users.splice(index, 1)
-    this.setState({
-      users: this.state.users
-    })
-  }
-
-  handleCheckboxChange() {
-    this.setState({
-      checked: !this.state.checked
-    })
-  }
   render() {
-    const userList = this.state.users.map((e, i) => <li key={i}>{e}<button onClick={this.deleteHandler.bind(this, i)}>Удолить</button></li>)
+    const options = colorsName.map((e, i) => <option value={i} key={i}>{e}</option>)
     return (
       <div className='block'>
-        <ul>
-          {userList}
-        </ul>
-      <form onSubmit={this.handleSubmit.bind(this)}>
-        <label>
-          <input type="text" name="name" value={this.state.value} onChange={this.onChangeHandler.bind(this)}/>
-          
-        </label>
-        <input type="submit" value="Отправить" />
-      </form>
-      <textarea value={this.state.value} onChange={this.onChangeHandler.bind(this)} />
-      <input
-        type="checkbox"
-        checked={this.state.checked}
-        onChange={this.handleCheckboxChange.bind(this)}
-      />
-    <p>{this.state.valueToShow}</p>
-    <p>Ваш выбор: {this.state.value1}</p>
-			<select
-				value={this.state.value1}
-				onChange={this.handleSelectChange.bind(this)}
-			>
-
-        <option value="0">Язык HTML</option>
-				<option value="1">Язык CSS</option>
-				<option value="2">Язык JavaScript</option>
-				<option value="3">Язык PHP</option>
-
-			</select>
+        <select
+          value={this.state.color}
+          onChange={this.changeColorHandler.bind(this)}
+        >{options}</select>
+        <p style={{color: colors[this.state.color]}}>{paragraphs[this.state.color]}</p>
     </div>
     )
   }   
