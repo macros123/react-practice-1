24fa13621c2253209dc9367176773eff21f7a9c9
macros123 <hit942010@gmail.com>
2020-04-28 23:49:46 +0300

    celendar

--- src/App.js
+++ src/App.js
@@ -1,32 +1,73 @@
 import React from 'react';
 import './App.css';
 
-const colors = ['black', 'blue', 'red', 'green']
-const colorsName = ['черный', 'синий', 'красный', 'зеленый']
-const paragraphs = ['111111111', '222222222', '33333333', '444444444444']
+const days = []
+for(let i = 0 ; i < 31; i++) {
+  days.push(i+1)
+}
+const month = []
+for(let i = 0 ; i < 12; i++) {
+  month.push(i+1)
+}
+const year = []
+for(let i = 1990 ; i < 2020; i++) {
+  year.push(i+1)
+}
+const date = new Date()
+const dayOfWeeks = ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота']
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      color: 0
+      input1: date.getDate(),
+      input2: date.getMonth() + 1,
+      input3: date.getFullYear(),
+      result: dayOfWeeks[date.getDay()]
     };
   }
  
-  changeColorHandler(event) {
-    this.setState({
-      color: event.target.value
-    })
+  changeTextHandler(event) {
+    const tmp = this.state
+    if(event.target.name === 'select1'){
+      const tmpDate = new Date(tmp.input3, tmp.input2 - 1, event.target.value).getDay()
+      this.setState({
+        input1: event.target.value,
+        result: dayOfWeeks[tmpDate]
+      })
+    } 
+    if(event.target.name === 'select2'){
+      const tmpDate = new Date(tmp.input3, event.target.value - 1, tmp.input1).getDay()
+      this.setState({
+        input2: event.target.value,
+        result: dayOfWeeks[tmpDate]
+      })
+    }
+    if(event.target.name === 'select3'){
+      const tmpDate = new Date(event.target.value, tmp.input2 - 1, tmp.input1).getDay()
+      this.setState({
+        input3: event.target.value,
+        result: dayOfWeeks[tmpDate]
+      })
+    }
+      
+    
   }
-
   render() {
-    const options = colorsName.map((e, i) => <option value={i} key={i}>{e}</option>)
+    const options1 = days.map((e, i) => <option key={i}>{e}</option>)
+    const options2 = month.map((e, i) => <option key={i}>{e}</option>)
+    const options3 = year.map((e, i) => <option key={i}>{e}</option>)
     return (
       <div className='block'>
-        <select
-          value={this.state.color}
-          onChange={this.changeColorHandler.bind(this)}
-        >{options}</select>
-        <p style={{color: colors[this.state.color]}}>{paragraphs[this.state.color]}</p>
+        <select name='select1' value={this.state.input1} onChange={this.changeTextHandler.bind(this)}>
+        {options1}
+        </select>
+        <select name='select2' value={this.state.input2} onChange={this.changeTextHandler.bind(this)}>
+        {options2}
+        </select>
+        <select name='select3' value={this.state.input3} onChange={this.changeTextHandler.bind(this)}>
+        {options3}
+        </select>
+    <p>{this.state.result}</p>
     </div>
     )
   }   
