882e3362cd7b56a3169c02ba01e4c7d3415d08df
macros123 <hit942010@gmail.com>
2020-04-28 09:27:54 +0300

    some inputs

--- src/App.js
+++ src/App.js
@@ -5,60 +5,78 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      show: false, 
-      name: 'Иван',
-      users: ['Коля', 'Вася', 'Петя', 'Иван', 'Дима'],
-      hrefs: [
-        {href: 'google.ru', text: 'ссылка 1'},
-        {href: '2.html', text: 'ссылка 2'},
-        {href: '3.html', text: 'ссылка 3'},
-      ]
+      value: '',
+      users: ['Петя', 'Вася', 'Анатолий', 'Анаколий'],
+      checked: false,
+      value1: '0'
     };
   }
-  
-  showMessage() {
+ 
+  onChangeHandler(event) {
     this.setState({
-      show: !this.state.show
-  })
+      value: event.target.value
+    })
   }
-
-  addPunkt() {
-    this.state.users.push('Пункт')
+  handleSelectChange(event) {
     this.setState({
-      users: this.state.users
+      value1: event.target.value
     })
   }
 
-  removePunkt() {
-    this.state.users.pop()
+  handleSubmit(event) {
+    this.state.users.push(this.state.value)
     this.setState({
       users: this.state.users
     })
+    event.preventDefault();
   }
 
-  removeOne(index) {
+  deleteHandler(event, index) {
     this.state.users.splice(index, 1)
-      this.setState({
-        users: this.state.users
-      })
-    
+    this.setState({
+      users: this.state.users
+    })
   }
 
+  handleCheckboxChange() {
+    this.setState({
+      checked: !this.state.checked
+    })
+  }
   render() {
-    const greetings = this.state.show ? 'Привет' : 'Пока'
-    const bod = this.state.show ? <p>{greetings},  {this.state.name}</p> : null
-    const bottonText = this.state.show ? 'скрыть' : 'показать' 
-  const userList = this.state.users.map((e, i) => <li key={i}>{e} - {i + 1}<button onClick={this.removeOne.bind(this, i)}>Удалить {e}</button></li>)
+    const userList = this.state.users.map((e, i) => <li key={i}>{e}<button onClick={this.deleteHandler.bind(this, i)}>Удолить</button></li>)
     return (
       <div className='block'>
-        {bod}
-    <button onClick={this.showMessage.bind(this)}>{bottonText}</button>
-    <ul>
-   {userList}
-    </ul>
-    <button onClick={this.addPunkt.bind(this)}>Добавить пункт</button>
-    <button onClick={this.removePunkt.bind(this)}>Удалить пункт</button>
-      </div>
+        <ul>
+          {userList}
+        </ul>
+      <form onSubmit={this.handleSubmit.bind(this)}>
+        <label>
+          <input type="text" name="name" value={this.state.value} onChange={this.onChangeHandler.bind(this)}/>
+          
+        </label>
+        <input type="submit" value="Отправить" />
+      </form>
+      <textarea value={this.state.value} onChange={this.onChangeHandler.bind(this)} />
+      <input
+        type="checkbox"
+        checked={this.state.checked}
+        onChange={this.handleCheckboxChange.bind(this)}
+      />
+    <p>{this.state.valueToShow}</p>
+    <p>Ваш выбор: {this.state.value1}</p>
+			<select
+				value={this.state.value1}
+				onChange={this.handleSelectChange.bind(this)}
+			>
+
+        <option value="0">Язык HTML</option>
+				<option value="1">Язык CSS</option>
+				<option value="2">Язык JavaScript</option>
+				<option value="3">Язык PHP</option>
+
+			</select>
+    </div>
     )
   }   
 }
